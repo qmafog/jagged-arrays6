4fbfec27f11832f3fd1ac422420b5599a8f362b4
qmafog <qmafog6@gmail.com>
2023-07-08 21:35:15 +0300

    done

--- JaggedArrays/ArrayExtension.cs
+++ JaggedArrays/ArrayExtension.cs
@@ -7,41 +7,184 @@ namespace JaggedArrays
         /// <summary>
         /// Orders the rows in a jagged-array by ascending of the sum of the elements in them.
         /// </summary>
-        /// <param name="source">The jagged-array to sort.</param>
+        /// <param name="array">The jagged-array to sort.</param>
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
-        public static void OrderByAscendingBySum(this int[][]? source)
+        public static void OrderByAscendingBySum(this int[][]? array)
         {
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            int n = array.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (GetSum(array[j]).CompareTo(GetSum(array[j + 1])) > 0)
+                    {
+                        int[] temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
         }
         
         /// <summary>
         /// Orders the rows in a jagged-array by descending of the sum of the elements in them.
         /// </summary>
-        /// <param name="source">The jagged-array to sort.</param>
+        /// <param name="array">The jagged-array to sort.</param>
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
-        public static void OrderByDescendingBySum(this int[][]? source)
+        public static void OrderByDescendingBySum(this int[][]? array)
         {
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            int n = array.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (GetSum(array[j]).CompareTo(GetSum(array[j + 1])) < 0)
+                    {
+                        int[] temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
         }
         
         /// <summary>
         /// Orders the rows in a jagged-array by ascending of the max of the elements in them.
         /// </summary>
-        /// <param name="source">The jagged-array to sort.</param>
+        /// <param name="array">The jagged-array to sort.</param>
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
-        public static void OrderByAscendingByMax(this int[][]? source)
+        public static void OrderByAscendingByMax(this int[][]? array)
         {
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            int n = array.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (GetMax(array[j]).CompareTo(GetMax(array[j + 1])) > 0)
+                    {
+                        int[] temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
         }
         
         /// <summary>
         /// Orders the rows in a jagged-array by descending of the max of the elements in them.
         /// </summary>
-        /// <param name="source">The jagged-array to sort.</param>
+        /// <param name="array">The jagged-array to sort.</param>
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
-        public static void OrderByDescendingByMax(this int[][]? source)
+        public static void OrderByDescendingByMax(this int[][]? array)
         {
-            throw new NotImplementedException();
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            int n = array.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (GetMax(array[j]).CompareTo(GetMax(array[j + 1])) < 0)
+                    {
+                        int[] temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
+
+        private static int GetSum(int[] array)
+        {
+            if (array is null)
+            {
+                return int.MinValue;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+
+        private static int GetMax(int[] array)
+        {
+            if (array is null)
+            {
+                return int.MinValue;
+            }
+
+            int max = int.MinValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
         }
     }
 }
